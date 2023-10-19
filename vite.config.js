74c1d56230816a283c65e90998ec7a1d8d263e76
ikagi <macsin777@gmail.com>
2023-10-19 20:38:59 +0200

    Update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3900,
+    port: 3899,
     host: true,
   },
   plugins: [react()],
