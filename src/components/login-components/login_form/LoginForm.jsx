d421395c1c63efd83c892345a3171e5a159bbeb5
ikagi <macsin777@gmail.com>
2023-10-21 22:09:12 +0200

    Update

--- src/components/login-components/login_form/LoginForm.jsx
+++ src/components/login-components/login_form/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './loginform.css';
 import { useNavigate } from 'react-router-dom';
-
+//const mysql = require("mysql2");
 export const LoginForm = () => {
   const navigate = useNavigate();
   const handleSubmit = (e) => {
